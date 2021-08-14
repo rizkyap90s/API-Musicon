ce6ddc6a93ca272ad0e5fb1a576bc60aece6a3ac
Adib Farrasy <adibfarrasy@gmail.com>
2021-08-14 09:05:44 +0700

    Updated song model

--- models/song.js
+++ models/song.js
@@ -34,6 +34,11 @@ const songSchema = new mongoose.Schema(
       required: true,
       default: "https://i.redd.it/pts7n7ojjol11.jpg",
     },
+    audio: {
+      type: String,
+      required: true,
+      default: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    },
   },
   {
     timestamps: {
