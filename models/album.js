0d525726354a4f90ff799b5e44ce9bee24d38c9c
Adib Farrasy <adibfarrasy@gmail.com>
2021-08-14 08:52:37 +0700

    Added album, playlist and song models

--- models/album.js
+++ models/album.js
@@ -1,1 +1,49 @@
 // Adib's Code
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+
+const albumSchema = new mongoose.Schema(
+  {
+    albumTitle: {
+      type: String,
+      required: true,
+      default: "Default Album",
+    },
+    songs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    ],
+    releaseDate: {
+      type: String,
+      required: true,
+      default: "1969",
+    },
+    artistId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Artist",
+    },
+    albumDuration: {
+      type: String,
+      required: true,
+      default: "6969",
+    },
+    albumImage: {
+      type: String,
+      required: true,
+      default: "https://i.redd.it/pts7n7ojjol11.jpg",
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+    toJSON: { getters: true },
+  }
+);
+
+albumSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+module.exports = mongoose.model("Album", albumSchema);
--- models/playlist.js
+++ models/playlist.js
@@ -1,1 +1,45 @@
 // Adib's Code
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+
+const playlistSchema = new mongoose.Schema(
+  {
+    playlistTitle: {
+      type: String,
+      required: true,
+      default: "Default Playlist",
+    },
+    songs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    ],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    playlistDuration: {
+      type: String,
+      required: true,
+      default: "6969",
+    },
+    playlistImage: {
+      type: String,
+      required: true,
+      default:
+        "https://i1.sndcdn.com/artworks-000560586507-q7vve7-t500x500.jpg",
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+    toJSON: { getters: true },
+  }
+);
+
+playlistSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+module.exports = mongoose.model("Playlist", playlistSchema);
