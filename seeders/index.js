65b097aff7fa629dce5d0ff6a1bef4fdbdc231e4
Adib Farrasy <adibfarrasy@gmail.com>
2021-08-18 14:29:56 +0700

    Updated seeders

--- seeders/index.js
+++ seeders/index.js
@@ -1,13 +1,19 @@
+const { Rating, Playlist } = require("../models");
 const { fetchArtists } = require("./artists");
 const { fetchAlbums } = require("./albums");
 const { fetchSongs } = require("./songs");
 const { populate } = require("./populate");
 const mongoose = require("mongoose");
-const { Rating } = require("../models");
 
 const add = async function () {
   try {
     await Rating.deleteMany();
+    const playlists = await Playlist.find();
+    for (const playlist of playlists) {
+      playlist.songs = [];
+      playlist.save();
+    }
+
     await fetchArtists();
     await fetchAlbums();
     await fetchSongs();
