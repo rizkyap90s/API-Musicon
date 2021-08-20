5061a0cdcc71008609bc14d898a540736448a2ee
Adib Farrasy <adibfarrasy@gmail.com>
2021-08-20 21:56:12 +0700

    Updated get songs by title logic

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,5 +1,5 @@
 // Adib's Code
-const { Song, Artist } = require("../models");
+const { Song, Artist, Album } = require("../models");
 
 class SongCtrl {
   async getDetailSong(req, res, next) {
@@ -26,8 +26,17 @@ class SongCtrl {
       const regex = new RegExp(req.query.title, "i");
 
       const songs = await Song.find({ songTitle: { $regex: regex } })
-        .populate({ path: "artistId", model: Artist })
-        .select("songTitle songImage artistId tags")
+        .populate({
+          path: "artistId",
+          model: Artist,
+          select: { _id: 1, name: 1, photo: 1 },
+        })
+        .populate({
+          path: "albumId",
+          model: Album,
+          select: { _id: 1, albumTitle: 1 },
+        })
+        .select("songTitle songImage artistId albumId tags")
         .skip(pageSize * (currentPage - 1))
         .limit(pageSize)
         .sort("-releaseDate");
