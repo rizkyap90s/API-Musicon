af04e1e8fdc7e4a128fb843b41d7ed685e2dd861
Adib Farrasy <adibfarrasy@gmail.com>
2021-08-26 18:56:15 +0700

    Updated spotify route

--- routes/auth/spotify.js
+++ routes/auth/spotify.js
@@ -4,27 +4,31 @@ require("dotenv").config({
 }); // Config environment
 
 const SpotifyWebApi = require("spotify-web-api-node");
-const querystring = require("querystring");
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  const code = req.query.code;
+
   const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   });
 
-  spotifyApi.clientCredentialsGrant().then(
-    function (data) {
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body["access_token"]);
-      res.status(200).json(data.body);
-    },
-    function (err) {
-      res.sendStatus(400);
-    }
-  );
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
 });
 
 module.exports = router;
