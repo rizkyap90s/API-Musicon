7e408a637669e8897eb544410843617945eec2e5
Adib Farrasy <adibfarrasy@gmail.com>
2021-09-05 13:49:16 +0700

    Updated facebook login v2

--- middlewares/auth/facebookV2.js
+++ middlewares/auth/facebookV2.js
@@ -17,9 +17,6 @@ exports.facebookLoginSanitizerValidator = async (req, res, next) => {
     }
 
     req.body.username = req.body.email.split("@")[0];
-    req.body.fullname = req.body.fullname;
-    req.body.photo = req.body.photo;
-    req.body.email = req.body.email;
     req.body.password = "facebook";
 
     next();
